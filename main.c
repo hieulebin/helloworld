3094de8dbf3bf202280cf61682b122c0116ce11e
= <=>
2017-10-16 16:43:03 +0700

    Hello World

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Nhap vao so nguyen a = ");
+    scanf("%d",&a);
+    printf("a = %d \n",a);
+
+    float f;
+    printf("Nhap vao mot so thuc f = ");
+    scanf("%f", &f);
+    printf("f = %f", f);
+    return 0;
+}
